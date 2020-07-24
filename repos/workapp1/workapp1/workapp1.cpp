cbcbe8e0aed196b2d4e598aa955f23e09830ba72
Bailiang <18762590320@163.com>
2020-07-24 14:18:55 +0800

    Site updated: 2020-07-24 14:18:54

---
+++ repos/workapp1/workapp1/workapp1.cpp
@@ -0,0 +1,8 @@
+﻿#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	
+	MessageBox(NULL, TEXT("这是我的第一个程序,你喜欢吗？"), TEXT("打招呼"), 1 | MB_ICONQUESTION);
+	return 0;
+}
